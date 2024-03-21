18f4468697c19d176ae59015c9d6536b28b7cc9c
luchovallejo <luchovallejo2627@gmail.com>
2024-03-20 19:39:39 -0500

    actualizacion 2.4

---
+++ View/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>EJEMPLO CRUD PHP ORM</title>
+    <link rel="stylesheet" href="../View/css/estilos.css"></link>
+</head>
+<body>
+    <center>
+            <table>
+                <tr>
+                    <th>MENU INVESTIGADORES</th>
+                </tr>
+                <tr>
+                    <td><a href="agregar.php">AGREGAR INVESTIGADOR</a></td>
+                </tr>
+                <tr>
+                    <td><a href="buscar.php">BUSCAR INVESTIGADOR</a></td>
+                </tr>
+                <tr>
+                    <td><a href="buscar.php">EDITAR INVESTIGADOR</a></td>
+                </tr>
+                <tr>
+                    <td><a href="buscar.php">ELIMINAR INVESTIGADOR</a></td>
+                </tr>
+                <tr>
+                    <td><a href="../Controllers/InvestigadorController.php?accion=todo">REPORTE DEL INVESTIGADOR</a></td>
+                </tr>
+            </table>
+    </center>
+</body>
+</html>
